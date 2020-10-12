d1d8fd66e5b306dead07b12be4f34df85b73e7f6
Matej <matejgeljic98@gmail.com>
2020-10-12 21:04:36 +0200

    redux implementation and converting to global state

--- client/src/pages/product-page/ProductPage.jsx
+++ client/src/pages/product-page/ProductPage.jsx
@@ -1,59 +1,68 @@
-import React, { useState, useEffect } from 'react';
-import { Link } from 'react-router-dom';
-import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Row, Col, Image, ListGroup, Button } from 'react-bootstrap';
 import Rating from '../../components/rating/Rating';
-import axios from 'axios';
+import Loader from '../../components/loader/Loader';
+import Message from '../../components/message/Message';
+import { listProductDetails } from '../../actions/productActions';
 
 const ProductPage = ({ match }) => {
-  const [product, setProduct] = useState({});
+  const dispatch = useDispatch();
+
+  const productDetails = useSelector((state) => state.productDetails);
+  const { loading, error, product } = productDetails;
 
   useEffect(() => {
-    const fetchProduct = async () => {
-      const { data } = await axios.get(`/api/products/${match.params.id}`);
-      setProduct(data);
-    };
-    fetchProduct();
-  }, [match]);
+    dispatch(listProductDetails(match.params.id));
+  }, [dispatch, match]);
+
+  console.log(product);
 
   return (
     <>
-      <Row>
-        <Col md={8}>
-          <Image src={product.image} alt={product.name} fluid />
-        </Col>
+      {loading ? (
+        <Loader />
+      ) : error ? (
+        <Message variant="danger">{error}</Message>
+      ) : (
+        <Row>
+          <Col md={8}>
+            <Image src={product.image} alt={product.name} fluid />
+          </Col>
 
-        <Col md={4}>
-          <ListGroup horizontal>
-            <ListGroup.Item style={{ borderWidth: 0 }}>
-              <h5>{product.name}</h5>
-            </ListGroup.Item>
-            <ListGroup.Item style={{ borderWidth: 0 }}>
-              <h5>${product.price}</h5>
-            </ListGroup.Item>
-          </ListGroup>
-          <ListGroup variant="flush">
-            <ListGroup.Item>
-              <Rating
-                value={product.rating}
-                text={` ${product.numReviews} reviews`}
-              />
-            </ListGroup.Item>
-            <ListGroup.Item>{product.description}</ListGroup.Item>
-            <ListGroup.Item>
-              {product.countInStock === 0 && (
-                <p className="text-center"> Out of stock</p>
-              )}
-              <Button
-                className="btn-block"
-                type="button"
-                disabled={product.countInStock === 0}
-              >
-                Add To Cart
-              </Button>
-            </ListGroup.Item>
-          </ListGroup>
-        </Col>
-      </Row>
+          <Col md={4}>
+            <ListGroup horizontal>
+              <ListGroup.Item style={{ borderWidth: 0 }}>
+                <h5>{product.name}</h5>
+              </ListGroup.Item>
+              <ListGroup.Item style={{ borderWidth: 0 }}>
+                <h5>${product.price}</h5>
+              </ListGroup.Item>
+            </ListGroup>
+            <ListGroup variant="flush">
+              <ListGroup.Item>
+                <Rating
+                  value={product.rating}
+                  text={` ${product.numReviews} reviews`}
+                />
+              </ListGroup.Item>
+              <ListGroup.Item>{product.description}</ListGroup.Item>
+              <ListGroup.Item>
+                {product.countInStock === 0 && (
+                  <p className="text-center"> Out of stock</p>
+                )}
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Col>
+        </Row>
+      )}
     </>
   );
 };
